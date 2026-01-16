d03909e4606e1465979eea2154058a3c1130696c
fzyzcjy <ch271828n@outlook.com>
2026-01-16 16:55:29 +0800

    more

--- miles/rollout/generate_hub/multi_turn_single_sample.py
+++ miles/rollout/generate_hub/multi_turn_single_sample.py
@@ -72,10 +72,9 @@ async def generate(input: GenerateFnInput) -> GenerateFnOutput:
 
         new_response_tokens = [item[1] for item in output["meta_info"]["output_token_logprobs"]]
         new_response_logprobs = [item[0] for item in output["meta_info"]["output_token_logprobs"]]
-        cur_response = tokenizer.decode(new_response_tokens)
 
         sample.tokens += new_response_tokens
-        sample.response += cur_response
+        sample.response += output["text"]
         sample.response_length += len(new_response_tokens)
 
         if sample.rollout_log_probs is None:
