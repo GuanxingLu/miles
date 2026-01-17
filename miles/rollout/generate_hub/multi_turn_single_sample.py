ecacbc9a1cc01b1fd1ad689056d7d6210a0fff13
fzyzcjy <ch271828n@outlook.com>
2026-01-17 08:16:17 +0800

    more

--- miles/rollout/generate_hub/multi_turn_single_sample.py
+++ miles/rollout/generate_hub/multi_turn_single_sample.py
@@ -71,7 +71,7 @@ def _add_arguments(parser: argparse.ArgumentParser):
     parser.add_argument("--generate-max-turns", type=int, default=16)
     parser.add_argument("--generate-tool-specs-path", type=str)
     parser.add_argument("--generate-tool-call-parser", type=str)
-    parser.add_argument("--generate-execute-tool-function-path", type=str)
+    parser.add_argument("--generate-multi-samples", type=bool, action="store_true")
 
 
 generate.add_arguments = _add_arguments
