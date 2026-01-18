8ed78e5244c15923649550d31859d41312ca3e4e
fzyzcjy <ch271828n@outlook.com>
2026-01-18 10:24:09 +0800

    more

--- miles/utils/arguments.py
+++ miles/utils/arguments.py
@@ -10,7 +10,7 @@ from transformers import AutoConfig
 
 from miles.backends.sglang_utils.arguments import add_sglang_arguments
 from miles.backends.sglang_utils.arguments import validate_args as sglang_validate_args
-from miles.utils.environ import get_experimental_rollout_refactor
+from miles.utils.environ import enable_experimental_rollout_refactor
 from miles.utils.eval_config import EvalDatasetConfig, build_eval_dataset_configs, ensure_dataset_list
 from miles.utils.logging_utils import configure_logger
 from miles.utils.misc import load_function
@@ -1390,7 +1390,7 @@ def get_miles_extra_args_provider(add_custom_arguments=None):
         parser = add_prefill_decode_disaggregation_arguments(parser)
         parser = add_ci_arguments(parser)
         parser = add_custom_megatron_plugins_arguments(parser)
-        if get_experimental_rollout_refactor():
+        if enable_experimental_rollout_refactor():
             parser = add_user_provided_function_arguments(parser)
         reset_arg(
             parser,
--- miles/utils/environ.py
+++ miles/utils/environ.py
@@ -1,5 +1,12 @@
 import os
 
+_printed_experimental_rollout_refactor = False
 
-def get_experimental_rollout_refactor() -> bool:
-    return bool(int(os.environ.get("MILES_EXPERIMENTAL_ROLLOUT_REFACTOR", "0")))
+
+def enable_experimental_rollout_refactor() -> bool:
+    global _printed_experimental_rollout_refactor
+    result = bool(int(os.environ.get("MILES_EXPERIMENTAL_ROLLOUT_REFACTOR", "0")))
+    if result and not _printed_experimental_rollout_refactor:
+        print("[MILES] MILES_EXPERIMENTAL_ROLLOUT_REFACTOR=1 is enabled (experimental feature)")
+        _printed_experimental_rollout_refactor = True
+    return result
